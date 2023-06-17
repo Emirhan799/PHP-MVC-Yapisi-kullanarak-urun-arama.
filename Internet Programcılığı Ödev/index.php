3656485ee75152bbbe50e37615b1e3e0df9c5395
Emirhan Aytekin <99148604+Emirhan799@users.noreply.github.com>
2023-06-17 21:40:32 +0300

    Delete Internet Programcılığı Ödev directory

--- Internet Programcılığı Ödev/index.php
+++
@@ -1,5 +0,0 @@
-<?php include 'control/control.php'; ?>
-
-<?php include 'model/model.php'; ?>
-
-<?php include 'view/view.php'; ?>
--- Internet Programcılığı Ödev/model/model.php
+++
@@ -1,4 +0,0 @@
-<form action="index.php" method="GET">
-    <input type="text" name="search" placeholder="Ürün Ara" value="<?php echo $search; ?>">
-    <button type="submit">Ara</button>
-</form>
